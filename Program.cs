6b84992ba5c352275206ff821d34ba5f288b0444
Asibongebhengu4 <asibongebhengu4@gmail.com>
2026-05-19 02:09:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter_Controlled_While_Loop_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStud,student=1,numM=0,numF=0,numErrors=0;
+            char Gender;
+            Console.Write("Hom many students ? ");
+            numStud = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter gender code of M,m or F,f for all " + numStud + " students");
+            while (student <= numStud)
+            {
+                Console.Write("\nStudent\t"+student+"\t:");
+                
+                Gender = Convert.ToChar(Console.ReadLine());
+                if (Gender == 'M' || Gender == 'm')
+                {
+                    numM++;
+                }
+                else if (Gender == 'F' || Gender == 'f')
+                {
+                    numF++;
+                }
+                else
+                {
+                    numErrors++;
+                }
+                student++;
+            }
+            Console.WriteLine("\nNumber of male: " + numM);
+            Console.WriteLine("Number of female: " + numF);
+            Console.WriteLine("Number of errors: " + numErrors);
+            Console.ReadLine();
+        }
+    }
+}
